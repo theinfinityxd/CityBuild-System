c170545be87367e7abc9c7cb002265383378762f
Baubo-LP <53007287+Baubo-LP@users.noreply.github.com>
2019-07-17 17:11:43 +0200

    Add files via upload

--- CityBuild System/src/BauboLP/CBSystem/Commands/FarmWeltCommand.php
+++ CityBuild System/src/BauboLP/CBSystem/Commands/FarmWeltCommand.php
@@ -6,6 +6,27 @@
  * Time: 21:08
  */
 
+/**
+ * @author BauboLP
+
+ * Copyright (c) 2019 BauboLP  < https://github.com/Baubo-LP >
+ * Discord: BauboLP#4545
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
+ *
+ * GunGame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ */
+
 namespace BauboLP\CBSystem\Commands;
 
 
--- CityBuild System/src/BauboLP/CBSystem/Commands/RemoveMoneyCommand.php
+++ CityBuild System/src/BauboLP/CBSystem/Commands/RemoveMoneyCommand.php
@@ -6,6 +6,27 @@
  * Time: 20:08
  */
 
+/**
+ * @author BauboLP
+
+ * Copyright (c) 2019 BauboLP  < https://github.com/Baubo-LP >
+ * Discord: BauboLP#4545
+ *
+ * This software is distributed under "GNU General Public License v3.0".
+ * This license allows you to use it and/or modify it but you are not at
+ * all allowed to sell this plugin at any cost. If found doing so the
+ * necessary action required would be taken.
+ *
+ * GunGame is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3.0 for more details.
+ *
+ * You should have received a copy of the GNU General Public License v3.0
+ * along with this program. If not, see
+ * <https://opensource.org/licenses/GPL-3.0>.
+ */
+
 namespace BauboLP\CBSystem\Commands;
 
 
@@ -23,7 +44,7 @@ class RemoveMoneyCommand extends Command
 
     public function execute(CommandSender $sender, string $commandLabel, array $args)
     {
-        if(!$sender->hasPermission("cb.set.money")) {
+        if(!$sender->hasPermission("cb.remove.money")) {
             $sender->sendMessage(CBSystem::Prefix.CBSystem::getPlugin()->getTextContainer("Player-has-no-perms"));
             return;
         }
